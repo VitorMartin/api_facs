4ab00c68b28f01de762e1467e1951cf2c0162d49
VitorMartin <vitor.martin.simoni@gmail.com>
2022-09-28 10:22:38 -0300

    Encapsulando app na funcao create_app

--- main.py
+++ main.py
@@ -19,34 +19,36 @@ print(f'Python version: {sys.version_info}')
 print(config.to_dict())
 
 
-@app.route('/', methods=['GET'])
-def get_home_endpoint():
-    return handler.get_home_handler()
+def create_app():
+    @app.route('/', methods=['GET'])
+    def get_home_endpoint():
+        return handler.get_home_handler()
 
 
-@app.route('/feeling', methods=['POST'])
-def post_feeling_endpoint():
-    if not request.mimetype.startswith('image/'):
-        return app.response_class(
-            response=json.dumps({'msg': 'Invalid MIME type provided. Send an image file instead.'}),
-            status=HTTP_400_BAD_REQUEST,
-            mimetype='application/json'
+    @app.route('/feeling', methods=['POST'])
+    def post_feeling_endpoint():
+        if not request.mimetype.startswith('image/'):
+            return app.response_class(
+                response=json.dumps({'msg': 'Invalid MIME type provided. Send an image file instead.'}),
+                status=HTTP_400_BAD_REQUEST,
+                mimetype='application/json'
+            )
+
+        return handler.post_feeling_handler(request)
+
+
+    @app.route('/feeling/img', methods=['POST'])
+    def post_image_endpoint():
+        return send_file(
+            handler.post_feeling_image_handler()
         )
 
-    return handler.post_feeling_handler(request)
 
-
-@app.route('/feeling/img', methods=['POST'])
-def post_image_endpoint():
-    return send_file(
-        handler.post_feeling_image_handler()
-    )
-
-
-@app.route('/feeling/all', methods=['GET'])
-def get_feeling_all():
-    return handler.get_feeling_all_handler()
+    @app.route('/feeling/all', methods=['GET'])
+    def get_feeling_all():
+        return handler.get_feeling_all_handler()
 
 
 if __name__ == '__main__':
+    create_app()
     app.run(host=config.HOST, port=config.PORT, debug=config.FLASK_DEBUG)
