cb4e0f158e13f6b3f346fcb84fa6855ccaccccf0
Rodolfo-Cochi <rodolfocochi@gmail.com>
2022-11-08 19:28:52 -0300

    criacao do predict dos modelos

--- src/api/handler.py
+++ src/api/handler.py
@@ -4,6 +4,11 @@ from src.config import Config
 
 import cv2 as cv
 import numpy as np
+import tensorflow as tf
+import os
+from keras.models import Model
+from keras.utils import img_to_array
+from keras.utils.image_utils import smart_resize
 
 from time import time
 
@@ -36,9 +41,100 @@ class Handler:
         feeling = df_predict['dominant_emotion']
 
         # Predict AUs
+        if(feeling == "happy"):
+            labels = ["6,12"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_happy.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        elif(feeling == "sad"):
+            labels = ["1,4,15", "1,4,15,17"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_sadness.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        elif(feeling == "disgust"):
+            labels = ["9,17", "10,17"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_disgust.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        elif(feeling == "fear"):
+            labels = ["1,2,4,5", "1,2,4,5,25"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_fear.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        elif(feeling == "angry"):
+            labels = ["4,5,7,17,23", "4,5,7,17,24"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_angry.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        elif(feeling == "surprise"):
+            labels = ["1,2,5,26", "1,2,26"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_surprise.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+        
+        else:
+            labels = ["0"]
+            au_list = []
+            curr_dir = os.path.dirname(os.path.abspath(__file__))
+            model = tf.keras.models.load_model(f'{curr_dir}/models/model_aus_neutral.h5')
+            img_keras = img_to_array(img_cv)
+            img_keras = np.expand_dims(img_keras, axis=0)
+            img_keras = smart_resize(img_keras, (224, 224))
+            prediction = model.predict(img_keras)[0]
+            prediction_idx = np.argmax(prediction)
+            prediction_label = labels[prediction_idx].split(",")
+            for au in prediction_label: (au_list.append(int(au)))
+          
         aus_payload = aus_decoder.get_feeling_payload(feeling)
         aus_payload.pop('feeling')
-        aus = aus_payload['aus']
+        aus = au_list #aus_payload['aus']
 
         # Draw AUs
         landmarks = aus_decoder.get_landmarks_from_aus(aus)
